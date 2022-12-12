7c1433e079101529f624172c8c31af8832d11e37
Chloe <chloevision97@gmail.com>
2022-12-12 06:33:19 +0100

    Ui Building fixing some Bugs

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-// for (let i = 1; i >= 9; i++) {
-//   const numb + i = 1;
-// }
-// let num1 = 0;
-// let num2 = 0;
+// variables
 let num1 = [];
 let num2 = [];
 let operation = [];
 let turn = 0;
 let operator = 1; // 1 add , 2 sub , 3 multi , 4 divi
 let repeat = 0;
-
-const test = [];
+// Dom selectors
 const numbs = document.querySelectorAll(`.numbBtn`);
 const addition = document.querySelector(`#add`);
 const resultbtn = document.querySelector(`#result`);
@@ -21,38 +16,30 @@ const divisonBtn = document.querySelector(`#divison`);
 const visor = document.querySelector(`.visor`);
 const delbtn = document.querySelector(`#del`);
 const historyvisor = document.querySelector(`.history`);
-// console.log(numbs.forEach((a) => test.push(a)));
-// console.log(test[0]);
 
-// const test1 = function () {
-//   console.log(this.value);
-// };
+// Numbers variable
 const value = function () {
-  if (operator === 0) {
+  if (repeat === 1 && operator === 0) {
     num1 = [];
     repeat = 0;
   }
-  //   if (turn === 0) {
-  //     num1 += this.value;
-  //   } else {
-  //     num2 += this.value;
-  //   }
-  if (num1.includes(`.`) && this.value === ".") return;
+  if (num1.includes(`.`) && this.value === "." && turn === 0) return;
   if (num2.includes(`.`) && this.value === ".") return;
   if (turn === 0) {
     num1.push(this.value);
     visor.textContent = num1.join("");
-    operation.push(num1.join(""));
+    operation.push(this.value);
   } else {
     num2.push(this.value);
     visor.textContent = num2.join("");
-    operation.push(num1.join(""));
+    operation.push(this.value);
   }
-  //   operation[0] = { value: [].push(this.value) };
 };
-numbs.forEach((e) => e.addEventListener(`click`, value));
-// test[0].addEventListener(`click`, test1);
 
+// Event for Numbers
+numbs.forEach((e) => e.addEventListener(`click`, value));
+
+// addition
 const add = function () {
   if (!num2.length == 0) {
     operate();
@@ -62,8 +49,10 @@ const add = function () {
   turn = 1;
   operator = 1;
   operation.push(" + ");
-  historyvisor.textContent = operation.join(" ");
+  historyvisor.textContent = operation.join("");
 };
+
+// Substraction , also - Sign
 const substr = function () {
   if (num1.length === 0 && turn === 0) {
     num1.push("-");
@@ -81,9 +70,10 @@ const substr = function () {
   turn = 1;
   operator = 2;
   operation.push(" - ");
-  historyvisor.textContent = operation.join(" ");
+  historyvisor.textContent = operation.join("");
 };
 
+// Multiply
 const multi = function () {
   if (!num2.length == 0) {
     operate();
@@ -93,8 +83,10 @@ const multi = function () {
   turn = 1;
   operator = 3;
   operation.push(" x ");
-  historyvisor.textContent = operation.join(" ");
+  historyvisor.textContent = operation.join("");
 };
+
+// Division
 const divison = function () {
   if (!num2.length == 0) {
     operate();
@@ -104,11 +96,14 @@ const divison = function () {
   turn = 1;
   operator = 4;
   operation.push(" / ");
-  historyvisor.textContent = operation.join(" ");
+  historyvisor.textContent = operation.join("");
 };
+
+// Operation
+
 const operate = function (e) {
   if (e !== undefined) {
-    historyvisor.textContent = operation.join(" ");
+    historyvisor.textContent = operation.join("");
   }
   if (operator === 1) {
     let result = parseFloat(num1.join("")) + parseFloat(num2.join(""));
@@ -152,14 +147,17 @@ const operate = function (e) {
     operator = 0;
   }
 };
-
+// Clear Button Function
 const clear = function () {
   num1 = [];
   num2 = [];
   turn = 0;
   visor.textContent = 0;
+  historyvisor.textContent = 0;
+  operation = [];
 };
 
+// Delete last digit
 const del = function () {
   if (turn === 0) {
     num1.pop();
@@ -169,7 +167,7 @@ const del = function () {
     visor.textContent = num2.join("");
   }
 };
-
+// Events for operation buttons
 addition.addEventListener(`click`, add);
 resultbtn.addEventListener(`click`, operate);
 multiBtn.addEventListener(`click`, multi);
@@ -178,7 +176,9 @@ subtraBtn.addEventListener(`click`, substr);
 divisonBtn.addEventListener(`click`, divison);
 delbtn.addEventListener(`click`, del);
 
-const test1 = [1, 2, 3];
-console.log(test1);
-console.log(parseFloat(test1.join("")));
-// This is test for redisign  number input
+// Year Footer
+const yeartxt = document.querySelector(`#year`);
+const now = new Date();
+const yearNow = now.getFullYear();
+
+yeartxt.textContent = yearNow;
