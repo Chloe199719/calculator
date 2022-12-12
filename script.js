a9e0f8f4ba6544934d7dfa623a801afce02783e2
Chloe <chloevision97@gmail.com>
2022-12-12 03:31:48 +0100

    value input reworked added new buttons

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // for (let i = 1; i >= 9; i++) {
 //   const numb + i = 1;
 // }
-
-let num1 = 0;
-let num2 = 0;
+// let num1 = 0;
+// let num2 = 0;
+let num1 = [];
+let num2 = [];
 let operation = [];
 let turn = 0;
 let operator = 1; // 1 add , 2 sub , 3 multi , 4 divi
@@ -16,6 +17,9 @@ const resultbtn = document.querySelector(`#result`);
 const resetbtn = document.querySelector(`#reset`);
 const subtraBtn = document.querySelector(`#subtra`);
 const multiBtn = document.querySelector(`#multi`);
+const divisonBtn = document.querySelector(`#divison`);
+const visor = document.querySelector(`.visor`);
+const delbtn = document.querySelector(`#del`);
 // console.log(numbs.forEach((a) => test.push(a)));
 // console.log(test[0]);
 
@@ -24,13 +28,22 @@ const multiBtn = document.querySelector(`#multi`);
 // };
 const value = function () {
   if (repeat === 1 && operator === 0) {
-    num1 = 0;
+    num1 = [];
     repeat = 0;
   }
+  //   if (turn === 0) {
+  //     num1 += this.value;
+  //   } else {
+  //     num2 += this.value;
+  //   }
+  if (num1.includes(`.`) && this.value === ".") return;
+  if (num2.includes(`.`) && this.value === ".") return;
   if (turn === 0) {
-    num1 += this.value;
+    num1.push(this.value);
+    visor.textContent = num1.join("");
   } else {
-    num2 += this.value;
+    num2.push(this.value);
+    visor.textContent = num2.join("");
   }
   //   operation[0] = { value: [].push(this.value) };
 };
@@ -38,7 +51,7 @@ numbs.forEach((e) => e.addEventListener(`click`, value));
 // test[0].addEventListener(`click`, test1);
 
 const add = function () {
-  if (!num2 == 0) {
+  if (!num2.length == 0) {
     operate();
     operator = 1;
     turn = 1;
@@ -47,7 +60,15 @@ const add = function () {
   operator = 1;
 };
 const substr = function () {
-  if (!num2 == 0) {
+  if (num1.length === 0 && turn === 0) {
+    num1.push("-");
+    return;
+  }
+  if (num2.length === 0 && turn === 1) {
+    num2.push("-");
+    return;
+  }
+  if (!num2.length == 0) {
     operate();
     operator = 2;
     turn = 1;
@@ -57,7 +78,7 @@ const substr = function () {
 };
 
 const multi = function () {
-  if (!num2 == 0) {
+  if (!num2.length == 0) {
     operate();
     operator = 3;
     turn = 1;
@@ -65,36 +86,45 @@ const multi = function () {
   turn = 1;
   operator = 3;
 };
+const divison = function () {
+  if (!num2.length == 0) {
+    operate();
+    operator = 4;
+    turn = 1;
+  }
+  turn = 1;
+  operator = 4;
+};
 const operate = function () {
   if (operator === 1) {
-    let result = parseFloat(num1) + parseFloat(num2);
-    console.log(result);
-    num1 = result;
-    num2 = 0;
+    let result = parseFloat(num1.join("")) + parseFloat(num2.join(""));
+    visor.textContent = result;
+    num1 = result.toString().split();
+    num2 = [];
     turn = 0;
     repeat = 1;
     operator = 0;
   } else if (operator === 2) {
-    let result = parseFloat(num1) - parseFloat(num2);
-    console.log(result);
-    num1 = result;
-    num2 = 0;
+    let result = parseFloat(num1.join("")) - parseFloat(num2.join(""));
+    visor.textContent = result;
+    num1 = result.toString().split();
+    num2 = [];
     turn = 0;
     repeat = 1;
     operator = 0;
   } else if (operator === 3) {
-    let result = parseFloat(num1) * parseFloat(num2);
-    console.log(result);
-    num1 = result;
-    num2 = 0;
+    let result = parseFloat(num1.join("")) * parseFloat(num2.join(""));
+    visor.textContent = result;
+    num1 = result.toString().split();
+    num2 = [];
     turn = 0;
     repeat = 1;
     operator = 0;
   } else if (operator === 4) {
-    let result = parseFloat(num1) / parseFloat(num2);
-    console.log(result);
-    num1 = result;
-    num2 = 0;
+    let result = parseFloat(num1.join("")) / parseFloat(num2.join(""));
+    visor.textContent = result;
+    num1 = result.toString().split();
+    num2 = [];
     turn = 0;
     repeat = 1;
     operator = 0;
@@ -102,17 +132,31 @@ const operate = function () {
 };
 
 const clear = function () {
-  num1 = 0;
-  num2 = 0;
+  num1 = [];
+  num2 = [];
   turn = 0;
+  visor.textContent = 0;
 };
+
+const del = function () {
+  if (turn === 0) {
+    num1.pop();
+    visor.textContent = num1.join("");
+  } else {
+    num2.pop();
+    visor.textContent = num2.join("");
+  }
+};
+
 addition.addEventListener(`click`, add);
 resultbtn.addEventListener(`click`, operate);
 multiBtn.addEventListener(`click`, multi);
 resetbtn.addEventListener(`click`, clear);
 subtraBtn.addEventListener(`click`, substr);
+divisonBtn.addEventListener(`click`, divison);
+delbtn.addEventListener(`click`, del);
 
-// const test1 = [1, 2, 3];
+// const test1 = ["-", 2, 3];
 // console.log(test1);
-// console.log(parseFloat(test1.join("")) + 1);
+// console.log(parseFloat(test1.join("")));
 // This is test for redisign  number input
